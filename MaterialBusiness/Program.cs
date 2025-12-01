3490e5aac57e4c677fe56ca6a6d22b9ee8edef2c
Sxoony <zamirkruger92@gmail.com>
2025-12-01 12:58:46 +0200

    Added faulty code

--- MaterialBusiness/Program.cs
+++ MaterialBusiness/Program.cs
@@ -5,10 +5,13 @@ using System.Linq;
 class Program
 {
     static Business business;
-
+    static List<Fabric> items = new List<Fabric>();
     static void Main(string[] args)
     {
-      
+
+        business = new Business("Fabric emporium", "123 jasmine lane");
+        items = business.Items.GetAllFabrics();
+
 
     }
 }
